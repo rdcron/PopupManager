2e7b6d0b46df514e41b743782deb0b840e03b9d5
rdcron <rdcron@gmail.com>
2023-09-27 19:57:40 -0500

    Add PresentationModeView to demo project

---
+++ PopupManagerExample/PopupManagerExample/Basics/PLPopup/PresentationModeView.swift
@@ -0,0 +1,20 @@
+//
+//  PresentationModeView.swift
+//  PopupManagerExample
+//
+//  Created by Richard Cron on 9/27/23.
+//
+
+import SwiftUI
+
+struct PresentationModeView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct PresentationModeView_Previews: PreviewProvider {
+    static var previews: some View {
+        PresentationModeView()
+    }
+}
