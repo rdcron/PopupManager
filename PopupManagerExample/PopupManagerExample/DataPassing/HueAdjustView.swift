fadbf4f147aa5759c935d040aaaaf72f95c1829d
rdcron <rdcron@gmail.com>
2023-09-28 21:06:47 -0500

    Added info popup link to explain PMSlider

--- PopupManagerExample/PopupManagerExample/DataPassing/HueAdjustView.swift
+++ PopupManagerExample/PopupManagerExample/DataPassing/HueAdjustView.swift
@@ -15,58 +15,77 @@ struct HueAdjustView: View {
     
     var body: some View {
         PopupView {
-            VStack(spacing: 40) {
-                VStack(spacing: 5) {
-                    Text("Hue")
+            ZStack {
+                VStack(spacing: 40) {
+                    VStack(spacing: 5) {
+                        Text("Hue")
+                        
+                        PMSlider(value: $hue, in: 0.0 ... 1.0) {
+                            Text("Hue adjustment slider")
+                        } minimumValueLabel: {
+                            Text("0.0")
+                        } maximumValueLabel: {
+                            Text("1.0")
+                        }
+                        .tint(Color("LinkYellow"))
+                    }
                     
-                    PMSlider(value: $hue, in: 0.0 ... 1.0) {
-                        Text("Hue adjustment slider")
-                    } minimumValueLabel: {
-                        Text("0.0")
-                    } maximumValueLabel: {
-                        Text("1.0")
+                    VStack(spacing: 5) {
+                        Text("Saturation")
+                        
+                        PMSlider(value: $sat, in: 0.0 ... 1.0) {
+                            Text("Saturation adjustment slider")
+                        } minimumValueLabel: {
+                            Text("0.0")
+                        } maximumValueLabel: {
+                            Text("1.0")
+                        }
+                        .tint(Color("LinkYellow"))
                     }
-                    .tint(Color("LinkYellow"))
-                }
-                
-                VStack(spacing: 5) {
-                    Text("Saturation")
                     
-                    PMSlider(value: $sat, in: 0.0 ... 1.0) {
-                        Text("Saturation adjustment slider")
-                    } minimumValueLabel: {
-                        Text("0.0")
-                    } maximumValueLabel: {
-                        Text("1.0")
+                    VStack(spacing: 5) {
+                        Text("Brightness")
+                        
+                        PMSlider(value: $bright, in: 0.0 ... 1.0) {
+                            Text("Brightness adjustment slider")
+                        } minimumValueLabel: {
+                            Text("0.0")
+                        } maximumValueLabel: {
+                            Text("1.0")
+                        }
+                        .tint(Color("LinkYellow"))
                     }
-                    .tint(Color("LinkYellow"))
-                }
-                
-                VStack(spacing: 5) {
-                    Text("Brightness")
                     
-                    PMSlider(value: $bright, in: 0.0 ... 1.0) {
-                        Text("Brightness adjustment slider")
-                    } minimumValueLabel: {
-                        Text("0.0")
-                    } maximumValueLabel: {
-                        Text("1.0")
+                    Button("Reset") {
+                        print("Reset")
+                        withAnimation {
+                            hue = 0.522
+                            sat = 0.72
+                            bright = 0.81
+                        }
                     }
-                    .tint(Color("LinkYellow"))
+                    .padding()
+                    .background(RoundedRectangle(cornerRadius: 5).fill(Color("PopupButtonBackground")))
                 }
+                .minimumScaleFactor(0.6)
                 
-                Button("Reset") {
-                    print("Reset")
-                    withAnimation {
-                        hue = 0.522
-                        sat = 0.72
-                        bright = 0.81
+                VStack {
+                    Spacer()
+                    HStack {
+                        Spacer()
+                        Image(systemName: "info.circle")
+                            .resizable()
+                            .frame(width: 20, height: 20)
+                            .linkFormat()
+                            .padding()
+                            .popupLink {
+                                InfoPopupView {
+                                    Text("SwiftUI's Slider doesn't seem to work well with this package, and after looking around on the internet, this seems to possibly be a bug with Slider itself. PopupManager intercepts all touches to keep track of the most recent touch location(this is necessary for some functions such as ad hoc popup presentation) before passing those touches to the enclosed views. So far this seems to work fine for everything except Slider. Therefore, the package includes PMSlider, which is made to be as close to the built-in slider as possible. Existing sliders should work as is just by importing PopupManager and putting 'PM' in front of the Slider declaration. Custom built sliders should also work fine.")
+                                }
+                            }
                     }
                 }
-                .padding()
-                .background(RoundedRectangle(cornerRadius: 5).fill(Color("PopupButtonBackground")))
             }
-            .minimumScaleFactor(0.6)
         }
     }
 }
