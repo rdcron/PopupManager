243443d5bd4f6ec833c21551614d6febe6d7c51d
rdcron <rdcron@gmail.com>
2023-09-28 20:58:33 -0500

    Added info popup to acknowledge John Sundell's 'Splash' package

--- PopupManagerExample/PopupManagerExample/Components/CodeBlock.swift
+++ PopupManagerExample/PopupManagerExample/Components/CodeBlock.swift
@@ -11,12 +11,46 @@ import Splash
 struct CodeBlock: View {
     let text: String
     
+    @State var blockSize = CGSize.zero
+    
     var body: some View {
-        Text(highlightText(text))
-            .padding()
-            .background(Color("AppBackground"))
-            .clipShape(RoundedRectangle(cornerRadius: 10))
+        ZStack {
+            Text(highlightText(text))
+                .padding()
+                .padding()
+                .background( GeometryReader { geo in
+                    Color("AppBackground")
+                        .preference(key: CodeWidthKey.self, value: geo.size)
+                }
+                )
+                .clipShape(RoundedRectangle(cornerRadius: 10))
+                .onPreferenceChange(CodeWidthKey.self) { newVal in
+                    blockSize = newVal
+                }
+                
+            VStack {
+                Spacer()
+                HStack {
+                    Spacer()
+                    Image(systemName: "info.circle")
+                        .resizable()
+                        .foregroundColor(Color("DarkText"))
+                        .frame(maxWidth: 30, maxHeight: 30)
+                        .padding()
+                        .foregroundColor(Color("LinkYellow"))
+                        .popupLink(widthMultiplier: 0.4, heightMultiplier: 0.4) {
+                            InfoPopupView {
+                                Text("Text highlighting is provided by John Sundell's 'Splash' Swift package.")
+                            }
+                        }
+                }
+                .frame(width: blockSize.width)
+            }
+            .frame(height: blockSize.height)
         }
+
+        
+    }
 
     
     func highlightText(_ input: String) -> AttributedString {
@@ -25,8 +59,11 @@ struct CodeBlock: View {
     }
 }
 
-struct CodeBlock_Previews: PreviewProvider {
-    static var previews: some View {
-        CodeBlock(text: "Test")
+struct CodeWidthKey: PreferenceKey {
+    typealias Value = CGSize
+    static var defaultValue: Value = .zero
+    
+    static func reduce(value: inout CGSize, nextValue: () -> CGSize) {
+        value = nextValue()
     }
 }
