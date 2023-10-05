6650ce1b37e6eb8fa40d1f2b1cd72d3ae6c153f1
rdcron <rdcron@gmail.com>
2023-10-04 20:41:44 -0500

    Added rotation detection modifier, but ended up not needing it. Might remove if no use is found.

---
+++ Sources/PopupManager/DeviceRotationViewModifier.swift
@@ -0,0 +1,26 @@
+//
+//  DeviceRotationViewModifier.swift
+//  
+//
+//  Created by Richard Cron on 10/3/23.
+//
+
+import SwiftUI
+
+struct DeviceRotationViewModifier: ViewModifier {
+    let action: (UIDeviceOrientation) -> Void
+
+    func body(content: Content) -> some View {
+        content
+            .onAppear()
+            .onReceive(NotificationCenter.default.publisher(for: UIDevice.orientationDidChangeNotification)) { _ in
+                action(UIDevice.current.orientation)
+            }
+    }
+}
+
+extension View {
+    func onRotate(perform action: @escaping (UIDeviceOrientation) -> Void) -> some View {
+        self.modifier(DeviceRotationViewModifier(action: action))
+    }
+}
