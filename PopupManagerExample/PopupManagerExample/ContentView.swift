4ae0bf105051dc373a303250658f641c5f746c76
rdcron <rdcron@gmail.com>
2023-10-06 15:10:30 -0500

    Update page layout

--- PopupManagerExample/PopupManagerExample/ContentView.swift
+++ PopupManagerExample/PopupManagerExample/ContentView.swift
@@ -5,56 +5,27 @@
 //  Created by Richard Cron on 9/17/23.
 //
 
-import PopupManager
-import MarkdownUI
 import SwiftUI
 
 
 struct ContentView: View {
     
     var body: some View {
-        GeometryReader { geo in
-            NavigationView {
-                PopupManager {
-                        ZStack {
-                            Color("AppBackground")
-                                .ignoresSafeArea()
-                            VStack {
-                                HStack {
-                                    IntroView()
-                                        .frame(width: geo.size.width * 0.45, height: geo.size.height * 0.4)
-//                                        .padding()
-                                    
-                                    BasicsView()
-                                        .frame(width: geo.size.width * 0.45, height: geo.size.height * 0.4)
-//                                        .padding()
-                                }
-                                .padding()
-                                
-                                HStack {
-                                    DismissView()
-                                        .frame(width: geo.size.width * 0.45, height: geo.size.height * 0.4)
-//                                        .padding()
-                                    
-                                    DataPassingView()
-                                        .frame(width: geo.size.width * 0.45, height: geo.size.height * 0.4)
-//                                        .padding()
-                                }
-//                                .padding()
-                            }
-                        }
-                        .frame(width: geo.size.width, height: geo.size.height)
+        TabView {
+            Page1View()
+                .tabItem {
+                    Label("Page 1", systemImage: "1.circle")
                 }
-                .navigationTitle("PopupManager")
-            }
-            .navigationViewStyle(.stack)
-//            .border(.red)
+            Page2View()
+                .tabItem {
+                    Label("Page 2", systemImage: "2.circle")
+                }
         }
     }
 }
 
-struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
-    }
-}
+//struct ContentView_Previews: PreviewProvider {
+//    static var previews: some View {
+//        ContentView()
+//    }
+//}
--- PopupManagerExample/PopupManagerExample/Page1/Page1View.swift
+++ PopupManagerExample/PopupManagerExample/Page1/Page1View.swift
@@ -6,10 +6,43 @@
 //
 
 import SwiftUI
+import PopupManager
 
 struct Page1View: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        GeometryReader { geo in
+            PopupManager {
+                    ZStack {
+                        Color("AppBackground")
+                            .ignoresSafeArea()
+                        VStack {
+                            HStack {
+                                IntroView()
+                                    .frame(width: geo.size.width * 0.45, height: geo.size.height * 0.4)
+//                                        .padding()
+                                
+                                BasicsView()
+                                    .frame(width: geo.size.width * 0.45, height: geo.size.height * 0.4)
+//                                        .padding()
+                            }
+                            .padding()
+                            
+                            HStack {
+                                DismissView()
+                                    .frame(width: geo.size.width * 0.45, height: geo.size.height * 0.4)
+//                                        .padding()
+                                
+                                DataPassingView()
+                                    .frame(width: geo.size.width * 0.45, height: geo.size.height * 0.4)
+//                                        .padding()
+                            }
+//                                .padding()
+                        }
+                    }
+                    .frame(width: geo.size.width, height: geo.size.height)
+            }
+            .navigationTitle("PopupManager")
+        }
     }
 }
 
